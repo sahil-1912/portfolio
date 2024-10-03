ad8542f54ab520c04d961e418608ce1f3997eb59
sahil <sahil00thakur1997gmail.com>
2024-10-03 13:32:05 +0530

    addig use

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from './app/App';
 import './index.css';
-
+//Github actions
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <App />
